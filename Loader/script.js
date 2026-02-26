63639f26db7b64575073b5f15cb0f55d96178fcd
aftereditchannel-cell <afteredit.channel@gmail.com>
2026-02-26 03:55:53 +0330

    Update script.js

--- Loader/script.js
+++ Loader/script.js
@@ -1,13 +1,1 @@
-<script>
-  // اگر صفحه Splash باز شد
-if (document.body.classList.contains('loader-container') || window.location.pathname.includes('Loader/index.html')) {
-  setTimeout(() => {
-    // مسیر صفحه بعدی رو اینجا بزار، فعلاً مثلا main.html
-    window.location.href = '../main.html';
-  }, 3000); // 3 ثانیه
-}
-  // بعد از 3 ثانیه به صفحه home.html برو
-  setTimeout(() => {
-    window.location.href = "home.html";
-  }, 3000);
-</script>
+
